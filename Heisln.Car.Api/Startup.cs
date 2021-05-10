1e3e8a4df3ae93c0fedb97379876dc194020e9ee
Johannes Schumeth <johannes.schumeth@gmail.com>
2021-05-10 15:23:33 +0200

    update swagger title

--- Heisln.Car.Api/Startup.cs
+++ Heisln.Car.Api/Startup.cs
@@ -42,7 +42,7 @@ namespace Heisln.Api
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Heisln.Car.Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Heisln UserService", Version = "v1" });
                 c.DescribeAllEnumsAsStrings();
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
                 {
@@ -115,7 +115,7 @@ namespace Heisln.Api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Heisln.Car.Api v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Heisln UserService v1"));
             }
 
             app.UseHttpsRedirection();
